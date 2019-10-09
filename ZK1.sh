e8c73f4ff511e208b5df6c007e4e29d8b56847a4
CLOWNSHAPPY <56341390+CLOWNSHAPPY@users.noreply.github.com>
2019-10-09 10:45:17 +0000

    Update ZK1.sh

--- ZK1.sh
+++ ZK1.sh
@@ -24,13 +24,13 @@ echo "\33[0;32m[13] \33[31;1mSedang Proses, Mohon Sabar"
 echo "\33[0;32m[14] \33[31;1mSedang Proses, Mohon Sabar"
 echo "\33[0;32m[15] \33[31;1mSedang Proses, Mohon Sabar"
 echo
-echo "\33[0;32m[99] \33[31;1mInstall Bahan"
+echo "\33[0;32m[99] \33[31;1mBaru Install Bahan Cok"
 echo "\33[0;32m[88] \33[31;1mKELUAR"
-echo "Install Bahan Dulu Supaya Gak Gagal"
+echo "Install Bahannya Dulu Biar Nggak Gagal Cok!!"
 echo
-read -p "Masukan Pilihan Anda :" roniyt
+read -p "pilihan mu apa yank :" Mrclown
 
-case $roniyt in
+case $Mrclown in
 1)
 clear
 sh spamroniyt.sh
@@ -45,7 +45,15 @@ python2 attack.py
 ;;
 4)
 clear
-sh vulnweb.sh
+pkg update
+pkg upgrade
+pkg install git
+pkg install php
+pkg install curl
+pkg install wget
+pkg install openssh
+git clone https://github.com/thelinuxchoice/shellphishsh 
+sh shellphish.sh
 ;;
 5)
 clear
@@ -95,6 +103,7 @@ echo "Author : ZK1_CYBER" | lolcat
 echo "[+] Perintah Ketik : nmap -v -A (link website) [+]" | lolcat
 echo "[!] Warning !! Setiap Detik IP Bisa Berubah - Ubah" | lolcat
 ;;
+
 99)
 clear
 pkg update
@@ -106,16 +115,16 @@ pip2 install lolcat
 pkg install git
 pkg install python
 pkg install python2
-sh ZK1.sh
+sh Mrclown.sh
 ;;
 88)
 clear
-echo "Berhasil Keluar"
-echo "Terimakasih Telah Memakai TOOL Saya"
+echo "Jangan Segan Buat Mampir Lagi"
+echo "I Hope You Can Enjoy"
 ;;
 *)
-echo "Maaf Nomor Yang Anda Ketik Salah"
+echo "Salah Ketik Nomer Gblk:v"
 sleep 5
-sh ZK1.sh
+sh Mrclown.sh
 ;;
 esac
